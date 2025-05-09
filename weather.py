eddf44d28b3d38be6ed131bb245fcc4da4076ca3
artinuwq <artinuwq@inbox.ru>
2025-05-09 12:44:32 +0300

    Функция get_weather теперь выдает даннные в виде словаря

--- weather.py
+++ weather.py
@@ -2,18 +2,49 @@ from config import AK_weather, city
 import requests
 url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid='+AK_weather
 
+icon = {
+    '01d': '☀️',  # Clear sky sun
+    '01n': '🌙',  # Clear sky moon
+    '02d': '🌤️',  # Few clouds sun
+    '02n': '🌥️',  # Few clouds moon
+    '03d': '☁️', '03n': '☁️',  # Scattered clouds
+    '04d': '☁️', '04n': '☁️',  # Broken clouds
+    '09d': '🌧️', '09n': '🌧️',  # Shower rain
+    '10d': '🌦️',  # Rain sun
+    '10n': '🌧️',  # Rain moon
+    '11d': '⛈️',  # Thunderstorm
+    '11n': '⛈️',  # Thunderstorm
+    '13d': '❄️',  '13n': '❄️',  # Snow
+    '50d': '🌫️',  # Mist
+    }
 
-weather_data = requests.get(url).json()
 
 
-temperature = round(weather_data['main']['temp'])
-temperature_feels = round(weather_data['main']['feels_like'])
-humidity = round(weather_data['main']['humidity'])
-wind_speed = round(weather_data['wind']['speed'])
+
+def get_weather():
+    weather_data = requests.get(url).json()
+
+
+    temperature = round(weather_data['main']['temp'])
+    temperature_feels = round(weather_data['main']['feels_like'])
+    humidity = round(weather_data['main']['humidity'])
+    wind_speed = round(weather_data['wind']['speed'])
+    weather_description = weather_data['weather'][0]['description']
+    icon = weather_data['weather'][0]['icon']
+
+    return {"temperature":temperature, 
+            'temperature_feels':temperature_feels, 
+            'humidity':humidity, 
+            'wind_speed':wind_speed, 
+            'weather_data':weather_data, 
+            'weather_description':weather_description, 
+            'icon':icon
+            }
 
 if __name__ == '__main__':
-    print(weather_data, '\n')
-    print('Сейчас в городе', city, str(temperature), '°C')
-    print('Ощущается как', str(temperature_feels), '°C')
-    print('Скорость ветра', str(wind_speed), '°C')
-    print('Влажность', str(humidity), '%') 
+    data = get_weather()
+    #print(data['weather_data'], '\n')
+    print('Погода в городе', city, '🏙️')
+    print('    ' + data['weather_description'].capitalize() + ' ' + icon[data['icon']])
+    print('    Температура:', data['temperature'], '°C')
+    print('    Ощущается как:', data['temperature_feels'], '°C')
